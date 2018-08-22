dd7b7b75faf1f4005b22a959400c5b95cc27a4f9
paulandreycabrera <paulandreycabrera@gmail.com>
2018-08-22 15:47:43 +0800

    First Commit

---
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+
+class CheckIfAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+     public function handle($request, Closure $next, $role)
+    {
+        if(
+        Auth::check() == false ||
+        Auth::user()->$role != "DEVELOPER" &&
+        Auth::user()->$role != "ADMIN") {
+            return redirect('/home');
+        }
+        return $next($request);
+    }
+}
---
+++ database/migrations/2018_08_22_060941_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('eventId');
+            $table->string('eventName');
+            $table->string('eventDate');
+            $table->string('eventVenue');
+            $table->string('eventTime');
+            $table->string('eventPhoto');
+            $table->integer('eventFee');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
